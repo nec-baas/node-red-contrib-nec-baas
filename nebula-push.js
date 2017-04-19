16edada1c4fdaf154c6581649bed421e389b938f
Kazuo Tsuge <tsuge@bx.jp.nec.com>
2017-04-19 12:28:49 +0900

    Improve the layout of push settings screen

--- nebula-push.js
+++ nebula-push.js
@@ -15,8 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-module.exports = function(RED) {
+
+module.exports = function (RED) {
     "use strict";
     const Common = require('./nebula-common');
        
